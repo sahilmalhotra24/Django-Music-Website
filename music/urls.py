9caf93fa63315564a7b273e8370dbca34521c172
Sahil Mangotra <sahilmangotra1997@gmail.com>
2017-02-12 21:23:46 +0530

    Added form in nav bar. Navbar completed by bootstrap.
    --> Hambuer issue

--- music/urls.py
+++ music/urls.py
@@ -5,7 +5,7 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index),
+    url(r'^$', views.index,name= 'index'),
 
     # music/45/
     url(r'^(?P<aid>[0-9]+)/$', views.detail, name='detail'),
