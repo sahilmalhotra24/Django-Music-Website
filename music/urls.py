589d9f29116e124dcd2a3c529f24fa72b7fa396e
Sahil Mangotra <sahilmangotra1997@gmail.com>
2017-02-24 00:44:23 +0530

    Added option to edit and delete an album

--- music/urls.py
+++ music/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
 
     #/music/album/add/
     url(r'album/add/$', views.AddCreate.as_view(), name='add-album'),
+
+    #/music/album/45/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    #/music/45/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delte'),
 ]
